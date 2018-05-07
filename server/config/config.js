45224b06df100fe769e57f30e057caf1e9f19a2d
Hugo Ortiz <hugo.er.ortiz@gmail.com>
2018-05-07 00:11:01 -0600

    Varaible de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -15,9 +15,9 @@ let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     // deberia ser local, pero no lo quiero instalar
-    urlDB = process.env.mongodb: //root:toor@ds217310.mlab.com:17310/cafe;
+    urlDB = process.env.MONGO_URI;
 } else {
-    urlDB = process.env.mongodb: //root:toor@ds217310.mlab.com:17310/cafe;
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
